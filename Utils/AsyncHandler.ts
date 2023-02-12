774958046beddecd69bf1167c7fe04f63ba1c6d5
nicsylvia <nicsylvia15f@gmail.com>
2023-02-12 15:40:15 -0800

    saved second asynchandler

--- Utils/AsyncHandler.ts
+++ Utils/AsyncHandler.ts
@@ -1,7 +1,7 @@
-import { Response, Request, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export const AsyncHandler = (fn: any) =>{
     return (req: Request, res: Response, next: NextFunction) =>{
-        Promise.resolve(fn(req, res, next)).catch(next);
+        Promise.resolve(fn(req, res, next)).catch(next)
     }
 }
