0d6e55c58486a2d157e0164dad83aa706c55f055
nicsylvia <nicsylvia15f@gmail.com>
2023-02-12 13:04:53 -0800

    saved all interfaces

---
+++ Models/AllInterfaces.ts
@@ -0,0 +1,16 @@
+import { Document } from "mongoose";
+
+export interface userData extends Document{
+    name: string,
+    email: string,
+    password: string,
+    confirmPassword: string,
+    role: string
+};
+
+export interface landData extends Document{
+    image: string,
+    price: string,
+    location: string,
+    views: string
+}
